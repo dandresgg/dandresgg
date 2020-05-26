7b964d01f1a32c4f9cab4edba61b834a3cbc8640
Diego Gomez <diegogomez268@gmail.com>
2020-05-26 08:28:41 -0500

    arreglo imagenes y envio correo4

--- send.php
+++ send.php
@@ -1,22 +1,31 @@
 <?php
-$name = $_POST['name'];
-$mail = $_POST['email'];
-// $empresa = $_POST['message'];
+$name = $email = $message = $para = $asunto = NULL;
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $mail = $_POST['email'];
 
-$header = 'From: ' . $mail . " \r\n";
-$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
-$header .= "Mime-Version: 1.0 \r\n";
-$header .= "Content-Type: text/plain";
+    $headers = 'From: ' . $mail . " \r\n";
+    $headers .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+    $headers .= "Mime-Version: 1.0 \r\n";
+    $headers .= "Content-Type: text/plain";
 
-$message = "Este mensaje fue enviado por " . $name . ",\r\n";
-$message .= "Su e-mail es: " . $mail . " \r\n";
-$message .= "Mensaje: " . $_POST['message'] . " \r\n";
-$message .= "Enviado el " . date('d/m/Y', time());
+    $message = "Este mensaje fue enviado por " . $name . ",\r\n";
+    $message .= "Su e-mail es: " . $mail . " \r\n";
+    $message .= "Mensaje: " . $_POST['message'] . " \r\n";
+    $message .= "Enviado el " . date('d/m/Y', time());
 
-$para = 'diegogomez268@gmail.com';
-$asunto = 'Mensaje de mi sitio web';
+    $para = 'diegogomez268@gmail.com';
+    $asunto = 'Mensaje de mi sitio web';
 
-mail($para, $asunto, utf8_decode($message), $header);
+    if (mail($para, $asunto, utf8_decode($message), $headers)) {
+        echo "<script language='javascript'>
+            alert('Mensaje enviado, muchas gracias.');
+         </script>";
+    } else {
+        echo "<script language='javascript'>
+            alert('fallado');
+         </script>";
+    }
 
-header("Location:index.html");
-?>
+    header("Location:index.html");
+}
